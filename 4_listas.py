d4546b82253ed5e987eafd5d55132a92e3719e35
Antônio Rocha <antoniojunior159@gmail.com>
2022-04-21 13:50:11 -0300

    Update 4_listas.py

--- 4_listas.py
+++ 4_listas.py
@@ -13,6 +13,58 @@ print (lista[0].title());
 del lista[2];
 print (lista);
 
+smash = "======================================="
+#Criando uma lista
+
+mylist = ["mortadela", "queijo", "ovo", "frango", "salsicha"]
+
+#Mostrando a lista na tela
+print(mylist)
+
+print(smash)
+
+#Acessando itens
+print(mylist[2])
+print(mylist[2:4])
+print(mylist[2].title()) #Acessando e colocando a 1º letra maiúscula
+
+print (smash)
+
+#Verificando se um item existe
+if "queijo" in mylist:
+	print("Sim! Queijo está na lista")
+	
+print (smash)
+
+#Mudando itens de uma lista
+mylist[0] = "presunto" #trocando mortadela por presunto
+print(mylist)
+
+print(smash)
+
+#Adicionando itens em uma lista 'append()'
+mylist.append("laranja")
+print(mylist)
+
+print(smash)
+
+#Adcionando itens no índice ordenado 'insert()'
+mylist.insert(3, "abacate") #Adicionando abacate no índice 3
+print (mylist)
+
+print(smash)
+
+#deletando item de uma lista
+del mylist[2]
+print (mylist)
+
+print(smash)
+
+#mostrando numero de itens da lista
+print(len(mylist))
+
+print(smash)
+
 #Ordenando a lista em ordem alfabética com a função Sort()
 
 car = ["yamaha", "susuki", "toyota", "chevrolet"];
