7280a1ea5bc608dd8313c55fc14b856a79cfd8a5
Antônio Rocha <antoniojunior159@gmail.com>
2022-04-22 15:05:32 -0300

    Update 4_listas.py

--- 4_listas.py
+++ 4_listas.py
@@ -8,10 +8,31 @@ print (lista);
 print (lista[0]);
 print (lista[0].title());
 
-#Excluir um item
+#Excluir um item da lista
 
 del lista[2];
 print (lista);
+
+#******CRIANDO LISTAS******
+
+lista = ["queijo", "mortadela", "batata", "leite"];
+print (lista);
+
+#Para buscar um item, digitar o índice que começa em 0
+
+print (lista[0]);
+print (lista[0].title());
+
+#Excluir um item da lista
+
+del lista[2];
+print (lista);
+
+#Ordenando a lista em ordem alfabética com a função Sort()
+
+car = ["yamaha", "susuki", "toyota", "chevrolet"];
+car.sort();
+print (car);
 
 smash = "======================================="
 #Criando uma lista
@@ -60,8 +81,56 @@ print (mylist)
 
 print(smash)
 
+#removendo item da lista por nome
+mylist.remove("abacate")
+
+print(smash)
+
 #mostrando numero de itens da lista
 print(len(mylist))
+
+print(smash)
+
+#limpando tudo em uma lista 'clear'
+mylist.clear()
+print(mylist)
+print("A lista está vazia!")
+
+print(smash)
+
+#Mostrando os itens da lista embaixo do outro
+newlist = ["python", "c#", "php", "java", "ruby"]
+for x in newlist:
+	print(x)
+	
+print(smash)
+
+#Copiando uma lista
+outherlist = newlist.copy()
+print(outherlist)
+
+print(smash)
+
+#Fundindo duas listas
+fusion = newlist + outherlist
+print(fusion)
+
+print(smash)
+#Percorrendo uma lista inteira com um laço
+
+magicos = ['Alice', 'David', 'Zatanna', 'Hollic', 'Kirk'];
+for magico in magicos: #Observe o uso do "dois pontos"
+	print (magico); #Observe a indentação
+
+#Criando Listas Numéricas (Função range () )
+
+for valor in range(1,6):
+	print (valor)
+
+#Série de Números
+
+numbers = list(range(1,6))
+print (numbers)
 
 print(smash)
 
@@ -70,7 +139,6 @@ print(smash)
 car = ["yamaha", "susuki", "toyota", "chevrolet"];
 car.sort();
 print (car);
-
 
 #Percorrendo uma lista inteira com um laço
 
@@ -88,5 +156,4 @@ for valor in range(1,6):
 numbers = list(range(1,6))
 print (numbers)
 
-
-
+print(smash)
